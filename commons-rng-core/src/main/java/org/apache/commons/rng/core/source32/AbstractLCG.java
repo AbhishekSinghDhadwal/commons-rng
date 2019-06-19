7fc26c95f01f1269cfa7e2f7a7f6959a657a082c
AbhishekSinghDhadwal <abhi1507@hotmail.com>
2019-06-19 20:04:07 +0530

    Commited checkstyle changes and added javadoc

--- commons-rng-core/src/main/java/org/apache/commons/rng/core/source32/AbstractLCG.java
+++ commons-rng-core/src/main/java/org/apache/commons/rng/core/source32/AbstractLCG.java
@@ -1,21 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.commons.rng.core.source32;
 
+import org.apache.commons.rng.core.util.NumberFactory;
+
+/**
+ * This abstract class is used in order to implement the LCG suite
+ * of generators, which trade predictability for speed, and use a simple formula
+ * in order to attain the next state , provided as follows :
+ *          next_state = ( a * current_state + c ) * m
+ * where a,c and m are user provided constants, respectively.
+ *
+ * Being primitive in nature, the usage of these generators should be avoided,
+ * or constrained to testing purposes.
+ *
+ * @see <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC285899/pdf/pnas00123-0038.pdf">
+ *  Marsaglia, G., (1968), "Random Numbers Fall Mainly in the Planes",
+ *  Proceedings of the National Academy of Sciences, 61:25-28. 2 </a>
+ *
+ * @since NOT RELEASED YET
+ */
+
 public abstract class AbstractLCG extends IntProvider {
-    private long a,c;
+
+    /** The multiplicative constant - a.*/
+    private long a;
+    /** The additive constant - c.*/
+    private long c;
+    /** The modulus utilised - m.*/
     private long m;
-    private int seed;
-    AbstractLCG()
-    {}
-    AbstractLCG(long a,long c,long m,int seed)
-    {
+    /** Defines the current state.*/
+    private int state;
+
+    /** Kindly ignore.*/
+    AbstractLCG(){}
+
+    /**
+     * Creates an instance with the given {@code seed}.
+     *
+     * @param a - The multiplicative constant.
+     * @param c - The additive constant.
+     * @param m - Modulus utilised.
+     * @param seed Initial seed.
+     */
+    AbstractLCG(long a, long c, long m, Integer seed) {
         this.a = a;
         this.c = c;
         this.m = m;
-        this.seed = seed;
+        /** Seed - The initial input for the generator. */
+        this.state = seed;
     }
-    public int next()
-    {
-        seed = (int) ((a * seed + c) % m);
-        return seed;
+
+    /**Utilises the formula provided for the LCG in order to return an integer.*/
+    @Override
+    public int next() {
+        state = (int) ((a * state + c) % m);
+        return state;
     }
+
+    /** {@inheritDoc} */
+    @Override
+    protected byte[] getStateInternal() {
+        return composeStateInternal(NumberFactory.makeByteArray(state),
+                super.getStateInternal());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void setStateInternal(byte[] s) {
+        final byte[][] d = splitStateInternal(s, 4);
+
+        state = NumberFactory.makeInt(d[0]);
+        super.setStateInternal(d[1]);
+    }
+
+
 }
--- commons-rng-core/src/main/java/org/apache/commons/rng/core/source32/KnuthLewisLCG.java
+++ commons-rng-core/src/main/java/org/apache/commons/rng/core/source32/KnuthLewisLCG.java
@@ -1,16 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.commons.rng.core.source32;
 
+/**
+ * This class implements an LCG based on the constants attained from
+ * <a href="https://e-maxx.ru/bookz/files/numerical_recipes.pdf">
+ * Numerical Recipes</a> as displayed in
+ * <a href="https://en.wikipedia.org/wiki/Linear_congruential_generator#Parameters_in_common_use">
+ * Wikipedia</a>.
+ *
+ * @since NOT RELEASED YET
+ */
+
 public class KnuthLewisLCG extends AbstractLCG {
-    KnuthLewisLCG(int seed)
-    {
-        super(1664525,1013904223,4294967296L,seed);
+
+    /**
+     * Creates a new random number generator.
+     *
+     * @param seed Initial seed.
+     */
+    public KnuthLewisLCG(Integer seed) {
+
+        super(1664525, 1013904223, 4294967296L, seed);
+
     }
-    public static void main(String [] args)
-    {
-        KnuthLewisLCG a = new KnuthLewisLCG(0);
-        for(int i=0;i<10;i++)
-        {
-            System.out.println(Integer.toHexString(a.next()));
-        }
-    }
+
 }
