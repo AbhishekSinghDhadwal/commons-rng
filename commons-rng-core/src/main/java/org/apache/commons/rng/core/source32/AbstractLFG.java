5b2652357272c480f044757f89889fcacd598ecf
AbhishekSinghDhadwal <abhi1507@hotmail.com>
2019-07-03 22:13:50 +0530

    Update AbstractLFG.java
    Added changes to masking and made checkstyle changes.

--- commons-rng-core/src/main/java/org/apache/commons/rng/core/source32/AbstractLFG.java
+++ commons-rng-core/src/main/java/org/apache/commons/rng/core/source32/AbstractLFG.java
@@ -19,55 +19,59 @@ package org.apache.commons.rng.core.source32;
 public abstract class AbstractLFG extends IntProvider {
 
     /** The short lag.*/
-    private int short_lag;
+    private int shortlag;
     /** The long lag.*/
-    private int long_lag;
+    private int longlag;
     /** The word length.*/
     private int w;
     /** Defines the current state.*/
-    private int state[];
+    private int[] state;
     /** Provides current value in array used.*/
     private int i;
-
+    /** Used to mask the output result.*/
+    private final int mask;
     /**
      * Creates an instance with the given {@code seed}.
      *
-     * @param short_lag The short lag.
-     * @param long_lag The long lag.
+     * @param shortlag The short lag.
+     * @param longlag The long lag.
      * @param w Word length.
      * @param seed Initial seed.
      */
-    AbstractLFG(int short_lag, int long_lag, int w, int[] seed) {
+    AbstractLFG(int shortlag, int longlag, int w, int[] seed) {
         //Note: Add means to switch length of lags if required.
-        this.short_lag = short_lag;
-        this.long_lag = long_lag;
-        this.w = w;
-        state = new int[this.long_lag];
+        longlag = Math.abs(longlag);
+        shortlag = Math.abs(shortlag);
+        this.shortlag = Math.min(longlag, shortlag);
+        this.longlag = Math.max(longlag, shortlag);
+        this.w = Math.abs(w);
+        state = new int[this.longlag];
         fillState(state, seed);
+        mask = (1 << this.w) - 1;
     }
 
     /**
      * Used to fill the LFG ring {@code state}.
      */
-    void fill(){
+    void fill() {
         // two loops to avoid costly modulo operations
-        {
-            for(int j = 0; j < short_lag; ++j) {
-                state[j] = (state[j] + state[j+(long_lag-short_lag)]) & w;}
-        }
-        for(int j = short_lag; j < long_lag; ++j) {
-            state[j] = (state[j] + state[j-short_lag]) & w;}
+        for (int j = 0; j < shortlag; ++j) {
+            state[j] = (state[j] + state[j + (longlag - shortlag)]) & mask; }
+
+        for (int j = shortlag; j < longlag; ++j) {
+            state[j] = (state[j] + state[j - shortlag]) & mask; }
         i = 0;
     }
 
-    public int next()
-    {
-        if(i >= long_lag){
-            fill();}
-        return state[i++];
-    }
+    /**
+     * Returns next value of ring as long as i is less than longlag.
+     * Else it shall refresh the ring and return the first value.
+     * @return state[i++]
+     */
+    public int next() {
+        if (i >= longlag) {
+            fill();
+        }
+        return state[i++]; }
 
-    /* Make two implementations, one having pre filling taking place,
-    *  and the other with filling performed simultaneously.
-    */
 }
